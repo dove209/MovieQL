9b48f1a97283a145208a0f55b84a33a509d9a328
dove209 <dove209@gmail.com>
2022-02-26 16:52:04 +0900

    Wrapping a REST API with GraphQL

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, scroe }) => addMovie(name, scroe),
-    deleteMovie: (_, { id }) => deleteMovie(id),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   },
 };
 
