0bcd11bd4d4214ac5f34aba45b53e9793cd082e4
dove209 <dove209@gmail.com>
2022-02-26 17:49:45 +0900

    getById 추가

--- graphql/db.js
+++ graphql/db.js
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+const API_URL_DETAIL = "https://yts.mx/api/v2/movie_details.json";
 
 export const getMovies = (limit, rating) => {
   let REQUEST_URL = API_URL;
@@ -12,4 +13,11 @@ export const getMovies = (limit, rating) => {
   return fetch(REQUEST_URL)
     .then((res) => res.json())
     .then((json) => json.data.movies);
+};
+
+export const getById = (id) => {
+  console.log(id);
+  return fetch(`${API_URL_DETAIL}?movie_id=${id}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movie);
 };
